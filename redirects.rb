55602f5c78a5d5045088e799889d8b0fab83f956
Melinda Gutermuth <mgutermuth@pivotal.io>
2019-09-11 15:56:02 -0400

    Update version number and URL [#168268953]

--- redirects.rb
+++ redirects.rb
@@ -3,5 +3,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect to the most recent version of om
-r301 %r{/pivotal/buildservice/(?![\d-]+)(.*)}, "/pivotal/buildservice/2-7/$1"
+# Redirect to the most recent version of Build Service
+r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-0-3/$1"
