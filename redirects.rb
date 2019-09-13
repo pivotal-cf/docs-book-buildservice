0b98224a7a01bb034dd5f8b190f84079d33475af
Landon Jarzynski <ljarzynski@pivotal.io>
2019-09-13 13:54:39 -0700

    remove redirs

--- redirects.rb
+++ redirects.rb
@@ -6,5 +6,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 # Redirect to the most recent version of Build Service
 # r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-0-3/$1"
 
-r301 %r{/build-service/0-0-3/(.*)}, '/platform/build-service/0-0-3/$1'
 
