8b32fe0746c58239c7b9fe7092487b2f57b36f9f
Severine Tymon <s.tymon@gmail.com>
2019-10-28 14:47:45 -0700

    make 0-0-4 the latest version [#169411373]

--- redirects.rb
+++ redirects.rb
@@ -3,8 +3,8 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect to the most recent version of Build Service
-# r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-0-3/$1"
-
 r301 %r{/build-service/(?![\d-]+)(.*)}, "/platform/build-service/$1"
+
+# Redirect to the most recent version of Build Service
+r301 %r{/platform/build-service/(?![\d-]+)(.*)}, "/build-service/0-0-4/$1"
 
