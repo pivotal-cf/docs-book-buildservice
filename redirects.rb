08edf3d61f9c83633073b49438aff6373e4765cd
Severine Tymon <s.tymon@gmail.com>
2019-10-28 15:21:56 -0700

    remove unused redirect, add latest version redirect [#169411373]

--- redirects.rb
+++ redirects.rb
@@ -3,8 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/build-service/(?![\d-]+)(.*)}, "/platform/build-service/$1"
-
 # Redirect to the most recent version of Build Service
 r301 %r{/platform/build-service/(?![\d-]+)(.*)}, "/platform/build-service/0-0-4/$1"
 
