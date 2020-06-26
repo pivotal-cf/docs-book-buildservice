df7a0fde5c2426e7532e9ba8f775a4de09b883b3
Tyler Phelan <tylerp@vmware.com>
2020-06-26 15:01:30 -0400

    Use 0.2.0 as latest version everywhere
    - Fix dropdown links

--- redirects.rb
+++ redirects.rb
@@ -4,4 +4,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 # Redirect to the most recent version of Build Service
-r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-1-0/$1"
+r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-2-0/$1"
