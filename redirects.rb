7bf6478e0d291a765e981eb864e1f52fe690387b
animatedmax <mhufnagel@pivotal.io>
2021-04-22 17:56:07 -0700

    [#177879616] only publish v1.1

--- redirects.rb
+++ redirects.rb
@@ -3,5 +3,3 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect to the most recent version of Build Service
-r301 %r{/build-service/(?![\d-]+)(.*)}, "build-service/1-1/$1"
