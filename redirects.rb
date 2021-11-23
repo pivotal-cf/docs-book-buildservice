618792159a84818ded4733bb6e4c2d01e546b177
jncd <jday@pivotal.io>
2021-11-23 14:14:30 -0800

    [#180366839]1-1 redirects in 1.1 book

--- redirects.rb
+++ redirects.rb
@@ -3,7 +3,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect to the most recent version of Build Service
+# Redirect from docs.pivotal.io/build-service/1-1 to docs.vmware.com build service docs
 
-r301 %r{latest/(.*)}, "/build-service/1-2/$1"
-r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/1-2/$1"
+r301  %r{/build-service/1-1/index.html}, "https://docs.vmware.com/en/Tanzu-Build-Service/1.1/vmware-tanzu-build-service-v11/GUID-docs-build-service-index.html"
+r301  %r{/build-service/1-1/(.*)}, "https://docs.vmware.com/en/Tanzu-Build-Service/1.1/vmware-tanzu-build-service-v11/GUID-$1"
