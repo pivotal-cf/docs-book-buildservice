bf1fe8be0e019d1a722c340b73037988920868a1
jncd <jday@pivotal.io>
2021-11-23 12:32:34 -0800

    [#180366839]1-2 redirects in 1.2 book

--- redirects.rb
+++ redirects.rb
@@ -12,8 +12,3 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 r301  %r{/build-service/1-2/index.html}, "https://docs.vmware.com/en/Tanzu-Build-Service/1.2/vmware-tanzu-build-service-v12/GUID-docs-build-service-index.html"
 r301  %r{/build-service/1-2/(.*)}, "https://docs.vmware.com/en/Tanzu-Build-Service/1.2/vmware-tanzu-build-service-v12/GUID-$1"
-
-# Redirect from docs.pivotal.io/build-service/1-1 to docs.vmware.com build service docs
-
-r301  %r{/build-service/1-1/index.html}, "https://docs.vmware.com/en/Tanzu-Build-Service/1.1/vmware-tanzu-build-service-v11/GUID-docs-build-service-index.html"
-r301  %r{/build-service/1-1/(.*)}, "https://docs.vmware.com/en/Tanzu-Build-Service/1.1/vmware-tanzu-build-service-v11/GUID-$1"
